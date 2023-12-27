fcd7e0a1efd713458f69d0db5c9c2257b7bc1a6b
Dibyalochan <dibyalochanparida@gmail.com>
2023-12-27 12:01:49 +0530

    changed database with sqllite

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,14 +1,14 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize('usersdb', 'root', 'root', {
-    logging:false,
-    dialect: 'mysql',
-  });
-  try {
-    sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  } 
+const sequelize = new Sequelize('test-db', 'user', 'pass', {
+  dialect: 'sqlite',
+  host: ':memory:'
+});
+try {
+  sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
 export default sequelize;
